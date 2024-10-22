63ec440609a747de5b9a95fde3d85965a4cd09a6
Vishal Kajale <vishal.kajale@hexafoldtech.com>
2024-10-22 18:55:23 +0530

    #completed node farm project.

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ output=output.replace(/{%FROM%}/g,product.from);
 output=output.replace(/{%NUTRIENTS%}/g,product.nutrients);
 output=output.replace(/{%QUANTITY%}/g,product.quantity);
 output=output.replace(/{%DESCRIPTION%}/g,product.description);
-output=output.replace(/{%ID%}/g,product.ID);
+output=output.replace(/{%ID%}/g,product.id);
 
 if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
 
@@ -61,21 +61,24 @@ return output;
 
 }
 const server = http.createServer((req,res)=>{
-    const pathName = req.url;
-    if(pathName==='/' || pathName === '/overview'){
-        res.writeHead(200,{
-            'Content-type':'text/html'
-        });
+    // const pathname = req.url;
 
-const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el)).join('');
-console.log(cardsHtml);
-const output = tempOverView.replace('{%PRODUCT_CARDS%',cardsHtml);
+const {query,pathname} =   url.parse(req.url,true);
+
+    if(pathname==='/' || pathname === '/overview'){
+
+    res.writeHead(200,{'Content-type':'text/html'});
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el)).join('');
+    const output = tempOverView.replace('{%PRODUCT_CARDS%',cardsHtml);
+    res.end(output);
+
+    }else if(pathname==='/product'){
+        res.writeHead(200,{'Content-type':'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct,product);
         res.end(output);
-
-    }else if(pathName==='/product'){
-        res.end('This is the PRODUCT');
     }
-    else if(pathName==='/api'){
+    else if(pathname==='/api'){
 
     res.writeHead(200,{  'Content-type':'application/json'});
 
