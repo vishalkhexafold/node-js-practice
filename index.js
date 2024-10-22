72255cf095da95ae62b1eff3b85154bb7ba5dcf3
Vishal Kajale <vishal.kajale@hexafoldtech.com>
2024-10-22 18:27:48 +0530

    added route and parse data .

--- index.js
+++ index.js
@@ -37,17 +37,55 @@ const url   = require('url');
 ///////////////////////////////////////////////////
 
 //SERVER
+
+const tempOverView = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`,'utf-8');
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`,'utf-8');
+
+const dataObj = JSON.parse(data);
+
+const replaceTemplate =(temp,product) => {
+let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+output=output.replace(/{%IMAGE%}/g,product.image);
+output=output.replace(/{%PRICE%}/g,product.price);
+output=output.replace(/{%FROM%}/g,product.from);
+output=output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+output=output.replace(/{%QUANTITY%}/g,product.quantity);
+output=output.replace(/{%DESCRIPTION%}/g,product.description);
+output=output.replace(/{%ID%}/g,product.ID);
+
+if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+
+return output;
+
+}
 const server = http.createServer((req,res)=>{
     const pathName = req.url;
     if(pathName==='/' || pathName === '/overview'){
-        res.end('THIS IS OVERVIEW');
+        res.writeHead(200,{
+            'Content-type':'text/html'
+        });
+
+const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el)).join('');
+console.log(cardsHtml);
+const output = tempOverView.replace('{%PRODUCT_CARDS%',cardsHtml);
+        res.end(output);
 
     }else if(pathName==='/product'){
         res.end('This is the PRODUCT');
-    }else{
+    }
+    else if(pathName==='/api'){
+
+    res.writeHead(200,{  'Content-type':'application/json'});
+
+    res.end(data);
+
+}
+    else{
         res.writeHead(404,{
             'Content-type':'text/html'
-            
+
 
         });
         res.end('<h1>Page not found!</h>');
